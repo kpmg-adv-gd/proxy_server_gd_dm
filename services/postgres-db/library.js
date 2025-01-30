708e62785f8c4dd0cbae8b6a6edfd95fd9518e67
lcardacciakpmg <lcardaccia@kpmg.it>
2025-01-30 18:29:50 +0100

    added variance labor

--- services/postgres-db/library.js
+++ services/postgres-db/library.js
@@ -1,6 +1,6 @@
 const getReasonsForVarianceQuery = "SELECT * FROM z_tipologia_varianza";
 
-const getMarkingDataQuery = `SELECT confirmation_number, planned_labor, marked_labor, remaining_labor
+const getMarkingDataQuery = `SELECT confirmation_number, planned_labor, marked_labor, remaining_labor, variance_labor
                                 FROM z_marcature
                                 WHERE project = $1 AND operation = $2`;
 
