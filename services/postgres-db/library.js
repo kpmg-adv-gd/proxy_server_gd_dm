380ffec6733376145f3ad7659e1176404157df01
lcardacciakpmg <lcardaccia@kpmg.it>
2025-01-31 15:21:36 +0100

    changed query

--- services/postgres-db/library.js
+++ services/postgres-db/library.js
@@ -1,12 +1,13 @@
-const getReasonsForVarianceQuery = "SELECT * FROM z_tipologia_varianza";
+const getReasonsForVarianceQuery = `SELECT * FROM z_variance_type`;
 
 const getMarkingDataQuery = `SELECT confirmation_number, planned_labor, marked_labor, remaining_labor, variance_labor
-                                FROM z_marcature
+                                FROM z_marking_recap
                                 WHERE project = $1 AND operation = $2`;
 
-const insertOpConfirmation = `INSERT INTO z_op_conferme (wbe_macchina, operation, mes_order, 
-                                confirmation_number, marking_date, start_time, finish_time, reason_for_variance)
-                                VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+const insertOpConfirmation = `INSERT INTO z_op_confirmations (wbe_macchina, operation, mes_order, 
+                                confirmation_number, marking_date, start_time, finish_time, marked_labor,
+                                uom_marked_labor, variance_labor, uom_variance_labor, reason_for_variance, user)
+                                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`;
 
 const getZOrdersLinkByProjectParentOrderChildOrderFlagQuery = `SELECT *
                                 FROM z_orders_link
