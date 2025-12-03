debf505c84051a4038f26637c14b6664f79e8614
Chiavaroli <mchiavaroli@KPMG.IT>
2025-12-03 13:07:54 +0100

    parametro 8-9

--- services/api/dataCollections/autoCompileDataCollections.js
+++ services/api/dataCollections/autoCompileDataCollections.js
@@ -26,13 +26,13 @@ async function autoCompileFieldsDataCollectionDispatcher(plant, data, parametriA
         // switch sulla funzione da chiamare
         switch (numParametro) {
             case "0":
-                data = await ruleParameter0(data, group, parameterName, selected, dcData, refresh);
+                data = await ruleParameter0(data, group, parameterName, dcData, refresh);
                 break;
             case "1":
                 data = await ruleParameter1(data, group, parameterName, selected, refresh);
                 break;
             case "2":
-                data = await ruleParameter2(data, group, parameterName, selected, dcData, refresh);
+                data = await ruleParameter2(data, group, parameterName, dcData, refresh);
                 break;
             case "3":
                 data = await ruleParameter3(data, group, parameterName, selected, refresh);
@@ -50,7 +50,10 @@ async function autoCompileFieldsDataCollectionDispatcher(plant, data, parametriA
                 data = await ruleParameter7(data, group, parameterName, selected, plant, refresh);
                 break;
             case "8":
-                data = await ruleParameter8(data, group, parameterName, selected, refresh);
+                data = await ruleParameter8(data, group, parameterName, selected, plant, refresh);
+                break;
+            case "9":
+                data = await ruleParameter9(data, group, parameterName, selected, plant, refresh);
                 break;
             default:
                 // Nessuna azione
@@ -60,7 +63,7 @@ async function autoCompileFieldsDataCollectionDispatcher(plant, data, parametriA
     return data;
 }
 
-async function ruleParameter0(data, group, parameterName, selected, dcData, refresh) {
+async function ruleParameter0(data, group, parameterName, dcData, refresh) {
     if (dcData.length > 0) {
         var filter2 = `(MFG_ORDER eq '${dcData[0].MFG_ORDER}' and DATA_FIELD eq 'CO')`;
         var mockReq2 = {
@@ -89,7 +92,7 @@ async function ruleParameter1(data, group, parameterName, selected, refresh) {
     for (var i = 0; i < data.length; i++) {
         if (data[i].group === group) {
             for (var j = 0; j < data[i].parameters.length; j++) {
-                    if (data[i].parameters[j].parameterName === parameterName && (data[i].parameters[j].valueText == "" || refresh)) {
+                if (data[i].parameters[j].parameterName === parameterName && (data[i].parameters[j].valueText == "" || refresh)) {
                     data[i].parameters[j].valueText = project;
                 }
             }
@@ -97,7 +100,7 @@ async function ruleParameter1(data, group, parameterName, selected, refresh) {
     }
     return data;
 }
-async function ruleParameter2(data, group, parameterName, selected, dcData, refresh) {
+async function ruleParameter2(data, group, parameterName, dcData, refresh) {
     if (dcData.length > 0) {
         var filter2 = `(MFG_ORDER eq '${dcData[0].MFG_ORDER}' and DATA_FIELD eq 'CUSTOMER')`;
         var mockReq2 = {
@@ -126,7 +129,7 @@ async function ruleParameter3(data, group, parameterName, selected, refresh) {
     for (var i = 0; i < data.length; i++) {
         if (data[i].group === group) {
             for (var j = 0; j < data[i].parameters.length; j++) {
-                    if (data[i].parameters[j].parameterName === parameterName && (data[i].parameters[j].valueText == "" || refresh)) {
+                if (data[i].parameters[j].parameterName === parameterName && (data[i].parameters[j].valueText == "" || refresh)) {
                     data[i].parameters[j].valueText = material;
                 }
             }
@@ -188,8 +191,112 @@ async function ruleParameter7(data, group, parameterName, selected, plant, refre
     }
     return data;
 }
-async function ruleParameter8(data, group, parameterName, selected, refresh) {
+async function ruleParameter8(data, group, parameterName, selected, plant, refresh) {
+    var optDaConsiderare = await getIncompleteOperations(plant, selected);
+    // Sommo i tempi PLAN_SETUP_TIME e PLAN_PROCESS_TIME di ogni orderScheduleData non presente in optNotIncluded
+    var totalTime = 0;
+    for (var i = 0; i < optDaConsiderare.length; i++) {
+        totalTime += (optDaConsiderare[i].PLAN_SETUP_TIME || 0) + (optDaConsiderare[i].PLAN_PROCESS_TIME || 0);
+    }
+    // Aggiorno il parametro
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].group === group) {
+            for (var j = 0; j < data[i].parameters.length; j++) {
+                if (data[i].parameters[j].parameterName === parameterName && (data[i].parameters[j].valueBoolean == "" || refresh)) {
+                    data[i].parameters[j].valueNumber = totalTime;
+                }
+            }
+        }
+    }
     return data;
+}
+async function ruleParameter9(data, group, parameterName, selected, plant, refresh) {
+    var optDaConsiderare = await getIncompleteOperations(plant, selected);
+    var operazioniRodaggioShared = await getZSharedMemoryData(plant,"OPERAZIONI_RODAGGIO");
+    try {
+        var operazioniRodaggio = operazioniRodaggioShared.length > 0 ? JSON.parse(operazioniRodaggioShared[0].value).values : [];
+    } catch (error) {
+        console.log("Errore nel parsing di OPERAZIONI_RODAGGIO da shared memory:", error);
+        return data;
+    }
+    // Verifico quante operazioni di rodaggio sono presenti in optDaConsiderare
+    var rigaTrovata = false;
+    for (var i = 0; i < optDaConsiderare.length; i++) {
+        for (var j = 0; j < operazioniRodaggio.length; j++) {
+            // controllo in like (contiene pezzo di stringa)
+            if (optDaConsiderare[i].OPERATION_ACTIVITY.includes(operazioniRodaggio[j])) {
+                rigaTrovata = true;
+                break;
+            }
+        }
+        if (rigaTrovata) {
+            break;
+        }
+    }
+    // Aggiorno il parametro
+    for (var i = 0; i < data.length; i++) {
+        if (data[i].group === group) {
+            for (var j = 0; j < data[i].parameters.length; j++) {
+                if (data[i].parameters[j].parameterName === parameterName && (data[i].parameters[j].valueBoolean == "" || refresh)) {
+                    data[i].parameters[j].valueBoolean = rigaTrovata ? "NO" : "SI";
+                }
+            }
+        }
+    }
+    return data;
+}
+
+// utils
+// Funzione per estrarre operazioni non concluse
+async function getIncompleteOperations(plant, selected) {
+    // Ricavo routing e routing_version
+    var ordersToCheck = await ordersChildrenRecursion(plant, selected.order);
+    const orderFilter = ordersToCheck.map(order => `MFG_ORDER eq '${order}'`).join(' or ');
+    var filter = `(PLANT eq '${plant}' and (${orderFilter}) )`;
+    var mockReq = {
+        path: "/mdo/ORDER",
+        query: { $apply: `filter(${filter})` },
+        method: "GET"
+    };
+    var outMock = await dispatch(mockReq);
+    var orderData = (outMock?.data?.value && outMock.data.value.length > 0) ? outMock.data.value : [];
+    if (orderData.length == 0) return [];
+    // prendo primo elemento e ci entro in mdo ROUTING_STEP
+    var filter2 = `(PLANT eq '${plant}' and ROUTING eq '${orderData[0].ROUTING}' and ROUTING_VERSION eq '${orderData[0].ROUTING_VERSION}')`;
+    var mockReq2 = {
+        path: "/mdo/ROUTING_STEP",
+        query: { $apply: `filter(${filter2})` },
+        method: "GET"
+    };
+    var outMock2 = await dispatch(mockReq2);
+    var routingStepData = (outMock2?.data?.value && outMock2.data.value.length > 0) ? outMock2.data.value : [];
+    // recupero la lista di operazioni (not null) in ROUTING_STEP_OPERATION_ACTIVITY: con queste entro in mdo ORDER_SCHEDULE per estrarre PLAN_SETUP_TIME e PLAN_PROCESS_TIME (da sommare poi)
+    var operationList = routingStepData.filter(rs => rs.ROUTING_STEP_OPERATION_ACTIVITY != null).map(rs => rs.OPERATION);
+    if (operationList.length > 0) {
+        var filter3 = `(PLANT eq '${plant}' and MFG_ORDER eq '${orderData[0].MFG_ORDER}' and ROUTING eq '${orderData[0].ROUTING}' and ROUTING_VERSION eq '${orderData[0].ROUTING_VERSION}'
+            and (${operationList.map(op => `OPERATION_ACTIVITY eq '${op}'`).join(' or ')}))`;
+        var mockReq3 = {
+            path: "/mdo/ORDER_SCHEDULE",
+            query: { $apply: `filter(${filter3})` },
+            method: "GET"
+        };
+        var outMock3 = await dispatch(mockReq3);
+        var orderScheduleData = (outMock3?.data?.value && outMock3.data.value.length > 0) ? outMock3.data.value : [];
+        // Estraggo operazioni da non includere nella somma finale dalla mdo SFC_STEP_STATUS
+        var filter4 = `(PLANT eq '${plant}' and MFG_ORDER eq '${orderData[0].MFG_ORDER}' and ROUTING eq '${orderData[0].ROUTING}' and ROUTING_VERSION eq '${orderData[0].ROUTING_VERSION}'
+            and (${operationList.map(op => `OPERATION_ACTIVITY eq '${op}'`).join(' or ')}))`;
+        var mockReq4 = {
+            path: "/mdo/ORDER_SCHEDULE",
+            query: { $apply: `filter(${filter4})` },
+            method: "GET"
+        };
+        var outMock4 = await dispatch(mockReq4);
+        var optNotIncluded = (outMock4?.data?.value && outMock4.data.value.length > 0) ? outMock4.data.value.filter(item => item.COMPLETED_AT != null) : [];
+        var optDaConsiderare = operationList.filter(op => !optNotIncluded.find(item => item.OPERATION_ACTIVITY === op));
+        return optDaConsiderare;
+    } else {
+        return [];
+    }
 }
 
 // Esporta la funzione
