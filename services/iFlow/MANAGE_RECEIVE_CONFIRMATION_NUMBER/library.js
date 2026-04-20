a5834abd7970e9bfaa1c8cfcd7c36f8c5180ed75
Chiavaroli <mchiavaroli@KPMG.IT>
2026-04-20 15:04:20 +0200

    fix campi json confNum

--- services/iFlow/MANAGE_RECEIVE_CONFIRMATION_NUMBER/library.js
+++ services/iFlow/MANAGE_RECEIVE_CONFIRMATION_NUMBER/library.js
@@ -6,6 +6,7 @@ const hostname = credentials.DM_API_URL;
 
 async function manageReceiveConfirmationNumber(jsonResponse) {
     var plant = await getPlantFromERPPlant(jsonResponse.plant);
+    console.log("INPUT DA SAP: " + JSON.stringify(jsonResponse));
     for (let k = 0; k < jsonResponse.orders.length; k++) {
         var operations = jsonResponse.orders[k].operations;
         // Step 1. chiamo api sfcdetails
@@ -26,18 +27,20 @@ async function manageReceiveConfirmationNumber(jsonResponse) {
                     var customValues = steps[m].routingOperation.customValues;
                     var confirmationNumberIndex = customValues.findIndex(obj => obj.attribute == "CONFIRMATION_NUMBER");
                     if (confirmationNumberIndex != -1) {
-                        customValues[confirmationNumberIndex].value = currentOperation.confirmationNumber;
+                        console.log("Custom trovato, aggiorno valore: " + customValues[confirmationNumberIndex].value + " con " + currentOperation.confirmation_number);
+                        customValues[confirmationNumberIndex].value = currentOperation.confirmation_number;
                     } else {
-                        customValues.push({ attribute: "CONFIRMATION_NUMBER", value: currentOperation.confirmationNumber });
+                        customValues.push({ attribute: "CONFIRMATION_NUMBER", value: currentOperation.confirmation_number });
                     }
                 }
             }
             // Scrivo in z_marking_recap per ogni operazione
-            await insertZMarkingRecap(plant, jsonResponse.orders[k].project, jsonResponse.orders[k].wbe, currentOperation.operation, jsonResponse.orders[k].order, currentOperation.confirmationNumber, currentOperation.duration, 
-                currentOperation.uomDuration, 0, currentOperation.uomDuration, currentOperation.duration, currentOperation.uomDuration, 0, currentOperation.uomDuration, currentOperation.operationDescription, false)
+            await insertZMarkingRecap(plant, jsonResponse.orders[k].project, jsonResponse.orders[k].wbe, currentOperation.operation, jsonResponse.orders[k].order, currentOperation.confirmation_number, currentOperation.duration, 
+                currentOperation.uom_duration, 0, currentOperation.uom_duration, currentOperation.duration, currentOperation.uom_duration, 0, currentOperation.uom_duration, currentOperation.operation_description, false)
         }
         // Step 4. aggiorno routing
         var urlUpdateRouting = hostname + "/routing/v1/routings";
+        console.log("ROUTING DA AGGIORNARE: " + JSON.stringify(responseRouting));
         await callPut(urlUpdateRouting, responseRouting);
     }
     // Tutto ok!
