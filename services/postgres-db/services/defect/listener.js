bb5e1a94da80640de7a69782cb23a20fac69613c
amancio11 <Mancio.10>
2026-05-07 12:26:58 +0200

    adjustmenet query defects

--- services/postgres-db/services/defect/listener.js
+++ services/postgres-db/services/defect/listener.js
@@ -138,12 +138,16 @@ module.exports.listenerSetup = (app) => {
             // Creo la query dinamina in base ai parametri ricevuti
             let query = "SELECT distinct z_defects.*, z_coding.coding, z_coding.coding_group, z_coding.coding_description, z_coding.coding_group_description, z_priority.description as priority_description, "
                         + "z_notification_type.description as notification_type_description, "
+                        + "z_verbale_lev_2.lev_1 as MACROFASE, z_verbale_lev_2.lev_2 as MACROATTIVITA, z_verbale_lev_3.lev_3 as CHECKLIST,"
                         + "COALESCE(z_responsible.org_level_4, COALESCE(z_responsible.org_level_3, COALESCE(z_responsible.org_level_2, COALESCE(z_responsible.org_level_1, '')))) as responsible_description "
                         + "FROM z_defects "
                         + "left join z_coding on z_defects.coding_id = z_coding.id " 
                         + "left join z_priority on z_defects.priority = z_priority.priority and z_defects.plant = z_priority.plant "
                         + "left join z_notification_type on z_defects.notification_type = z_notification_type.notification_type "
                         + "left join z_responsible on z_defects.responsible = z_responsible.id "
+                        + "left join z_defect_testing on z_defects.id = z_defect_testing.defect_id and z_defect_testing.plant = z_defects.plant "
+                        + "left join z_verbale_lev_2 on z_verbale_lev_2.id_lev_1 = z_defect_testing.id_lev_1 and z_verbale_lev_2.id_lev_2 = z_defect_testing.id_lev_2 AND z_verbale_lev_2.plant = z_defects.plant AND z_verbale_lev_2.sfc = z_defects.sfc "
+                        + "left join z_verbale_lev_3 on z_verbale_lev_3.id_lev_3 = z_defect_testing.id_lev_3 and AND z_verbale_lev_3.plant = z_defects.plant AND z_verbale_lev_3.sfc = z_defects.sfc "
                         + "WHERE z_defects.plant = '" + plant + "' ";
             if (wbe) {
                 query += ` AND z_defects.wbe = '${wbe}'`;
